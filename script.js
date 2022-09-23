2cad8f22674d6234685fb445701f0580976e237e
alan <alan@iMac-de-alan.local>
2022-09-23 17:54:52 -0500

    Computer chooses a random value

---
+++ script.js
@@ -0,0 +1,12 @@
+
+function getComputerChoice() { 
+let myGame = ['rock', 'paper', 'scissors']
+let randomValue = myGame[Math.floor(Math.random() * myGame.length)];
+console.log(randomValue + " computers choice")
+}
+getComputerChoice()
+
+
+
+
+
