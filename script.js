973f0b62e2a64a9a8cba6e586d5a5940ea2d6a71
alan <alan@iMac-de-alan.local>
2022-09-26 12:31:32 -0500

    Add if else statements to declare a winner

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
 
-function getComputerChoice() { 
-let myGame = ['rock', 'paper', 'scissors']
-let randomValue = myGame[Math.floor(Math.random() * myGame.length)];
-console.log(randomValue + " computers choice")
-}
-getComputerChoice()
 
 
-function playersChoice (){
-    let play = prompt("rock, paper or scissors?")
-    console.log(play + " players choice")
+function game (){ 
+   
+ let player = prompt("rock, paper or scissors?")
+    console.log("player choice: " + player)
+
+    let myGame = ['rock', 'paper', 'scissors']
+    let computer = myGame[Math.floor(Math.random() * myGame.length)];
+    console.log("computer choice: " + computer)
+   
+
+
+    if(player == computer){
+        console.log("Draw!") ;
+    }
+    else if (computer == "rock"){
+      return (player == "paper") ? console.log("You win, paper beats rock") : console.log("You lose, rock beats scissors")
+    }
+    else if (computer == "paper"){
+        return (players == "scissors") ? console.log("You win, scissors beats paper") :console.log("You lose, paper beats beats")
+      }
+    else if (computer == "scissors"){
+        return (player == "rock") ? console.log("You win, rock beats scissors") : console.log("You lose, scissors beats paper")
+      }
+
 }
-playersChoice()
+game()
